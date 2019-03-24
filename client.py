43c778b3328b8700f030d87110228516f379d3be
wori0702 <46152974+wori0702@users.noreply.github.com>
2019-03-24 18:54:28 +0900

    Add files via upload

--- client.py
+++ client.py
@@ -1,18 +1,30 @@
 import socket
 import argparse
+import glob
+import os
 
-def run(host, port):
+def run(host, port, file):
     with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
         s.connect((host, port))
-        line = input(':')
-        s.sendall(line.encode())
-        resp = s.recv(1024)
-        print(resp.decode())
+        s.sendall(file.encode())
+        
+        data= s.recv(1024)
+        if not data:
+                print("file no exist")
+                return
+        with open('./' + file, 'wb') as f:
+                file_size = int(data.decode())
+                data = s.recv(file_size)
+                try:
+                        f.write(data)
+                except Exception as e:
+                        print(e)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description="Echo Client -p port -i host")
     parser.add_argument('-p', help="port_number", required = True)
     parser.add_argument('-i', help="host_name", required = True)
+    parser.add_argument('-f', help="file_name",required = True)
 
     args = parser.parse_args()
-    run(host=args.i,port=int(args.p))
+    run(host=args.i,port=int(args.p),file=args.f)
--- server.py
+++ server.py
@@ -1,27 +1,38 @@
 import socket
 import argparse
+import os
+import glob
 
-def run_server(port=4000):
+def run_server(port=4000,file_dir='*'):
     host = ''
-
 
     with socket.socket(family=socket.AF_INET, type = socket.SOCK_STREAM) as s:
         s.bind((host,port))
         s.listen(1)
+        file_list_link = file_dir + "/*"
+        file_list=glob.glob(file_list_link)
+        for i in file_list:
+                print(i)
 
-        conn, addr = s.accept()
+        conn, addr = s.accept() 
         msg = conn.recv(1024)
-        print(msg.decode())
+        file_name = msg.decode()
+        link = file_dir + "\\" + file_name
+        if link in file_list:
+                file_size= str(os.path.getsize(link))
+                conn.sendall(file_size.encode())
+                with open(link, 'rb')as f:
+                        files = f.read(int(file_size))
+                        conn.send(files)
+        else:
+                print("file no exist")
 
-        msg = msg[::-1]
-
-        conn.sendall(msg)
         conn.close()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Echo server -p port")
+    parser = argparse.ArgumentParser(description="Echo server -p port -d direct")
     parser.add_argument('-p', help="port_number", required = True)
+    parser.add_argument('-d',help="file_dir", required = True)
 
     args = parser.parse_args()
-    run_server(port=int(args.p))
-    
+    run_server(port=int(args.p),file_dir = args.d)
